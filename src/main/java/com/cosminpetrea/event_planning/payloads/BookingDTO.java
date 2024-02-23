faa697fdee40444aeeeed8deda7a23b26f0c70d9
Cosmin <kosmyn1994@gmail.com>
2024-02-23 12:35:01 +0100

    Payloads done

---
+++ src/main/java/com/cosminpetrea/event_planning/payloads/BookingDTO.java
@@ -0,0 +1,18 @@
+package com.cosminpetrea.event_planning.payloads;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record BookingDTO(
+        @NotNull(message = "Event Id is mandatory")
+        UUID event,
+        @NotNull(message = "number of participants is mandatory")
+        int seatsNumber,
+        @NotNull(message = "User Id is mandatory")
+        UUID user,
+        @NotEmpty(message = "Date is mandatory")
+        LocalDate data) {
+}
---
+++ src/main/java/com/cosminpetrea/event_planning/payloads/EventDTO.java
@@ -0,0 +1,22 @@
+package com.cosminpetrea.event_planning.payloads;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record EventDTO(@NotEmpty(message = "Title is mandatory")
+                       @Size(min = 3, max = 20, message = "This title must have at least 3 to maximum 20 characters")
+                       String title,
+                       @NotEmpty(message = "Description is mandatory")
+                       @Size(min = 3, max = 50, message = "description must have at least 3 to maximum 50 characters")
+                       String description,
+                       @NotEmpty(message = "Date is mandatory")
+                       LocalDate date,
+                       @NotEmpty(message = "Location is mandatory")
+                       @Size(min = 3, max = 30, message = "Location must have at least 3 characters and max 30")
+                       String location,
+                       @NotNull(message = "Participants number is mandatory")
+                       Integer maxParticipants) {
+}
